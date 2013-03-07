d64ec9a15ee4bca5d0ee2f1afc46c05cdfa35472
dxw <xuewu.dai@eng.ox.ac.uk>
2013-03-07 17:41:55 +0000

    tempearary files are stored at dump folder at beagle side

--- work_gfit/plotSWNDAQ.m
+++ work_gfit/plotSWNDAQ.m
@@ -9,6 +9,12 @@ fnamescan=strcat(fnamebase,'scan.txt');
 fnameir=strcat(fnamebase,'ir.txt');
 fnameuv=strcat(fnamebase,'uv.txt');
 
+
+fnamescan='dump/refscan_20130307_13h53m09s.txt';
+fnameir='dump/refir_20130307_13h53m09s.txt';
+fnameuv='dump/refuv_20130307_13h53m09s.txt';
+
+
 localFolder='E:\Beagle\Eclipseworkspace\MatlabCoder_1b\work_gfit\datafile';
 remotefilename=strcat('/home/ubuntu/Loadmon_BeaglePlus/',fnamescan);
 fprintf('Fetch remote file %s\n', remotefilename);
@@ -20,8 +26,8 @@ plotscandata(fnamescan,'.\datafile');
 
 % fnameir=strcat(fnamebase,'ir3.txt');
 % fnameuv=strcat(fnamebase,'uv.txt');
-% fnameir='wtr100.txt';
-% fnameuv='wtr140uv2.txt';
+% fnameir='fname.txt';
+% fnameuv='dump/refuv_20130307_00h25m04s.txt';
 remotefilename=strcat('/home/ubuntu/Loadmon_BeaglePlus/',fnameir);
 fprintf('Fetch remote file %s\n', remotefilename);
 scptomatlab('ubuntu','10.2.1.239','temppwd',localFolder,remotefilename);
@@ -42,61 +48,9 @@ grid on;
 %  3. open the file data_ap.txt and remove the firs line 'swn xx xx xx'
 %  4. save the modified file
 %  5. call plotdatadata('data_ap.txt') to plot the mean value
-plotdaqdata('data_ap.txt');
-grid on;
 
-
-%%  plot datalog.txt file
-fdata='datalog_20130306.txt';
-localFolder='E:\Beagle\Eclipseworkspace\MatlabCoder_1b\work_gfit\datafile';
-remotefilename=strcat('/home/ubuntu/Loadmon_BeaglePlus/data/',fdata);
-
-fprintf('Fetch remote file %s\n', remotefilename);
-scptomatlab('ubuntu','10.2.1.239','temppwd',localFolder,remotefilename);
-fprintf('\n');
-
-localfile=strcat(strcat(localFolder,'\'),fdata);
-dlog=importdata(localfile, '\t');
-[nRow,nCol]=size(dlog);
+% plotdaqdata('data_ap.txt');
+% grid on;
 
 
 
-hh=dlog.data(:,1); mmin=dlog.data(:,2); ssec=dlog.data(:,3);
-tempC=dlog.data(:,26);
-apdbv=dlog.data(:,27);
-aomv=dlog.data(:,28);
-
-sInterval=60;% second
-hhdiff=diff(hh);
-hhdiffid=find(hhdiff);
-startid=hhdiffid(1)+1;
-starthh=hh(startid);
-
-nHour=length(hhdiffid);
-
-hhlast = find(hhdiff,1,'last')
-endid=hhlast+1;
-endhh=hh(endid);
-hhsym=num2str([starthh:endhh]');
-hhid=linspace(startid,endid,nHour);
-
-figure('name',strcat('TempC,APDbv, aomv_',fdata));
-subplot(3,1,1);
-plot(tempC,'.-r');
-grid on;
-xlabel('Time (Hour)');
-ylabel('Temperature (degC)');
-set(gca,'XTick',hhid)
-set(gca,'XTickLabel',hhsym)
-titleStr={'Temperature Measurement and Compensation',fdata};
-title(titleStr,'Interpreter','none','fontsize', 14);
-
-subplot(3,1,2);
-plot(apdbv,'.-r');  title('Expected APD bias voltage');
-xlabel('Time (Hour)'); ylabel('APD bias voltage (V)'); grid on;
-set(gca,'XTick',hhid); set(gca,'XTickLabel',hhsym);
-
-subplot(3,1,3);
-plot(aomv,'.-b');  title('Analogue output to be set');
-xlabel('Time (Hour)'); ylabel('analogue output (mV)'); grid on;
-set(gca,'XTick',hhid); set(gca,'XTickLabel',hhsym);
